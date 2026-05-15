ff789995493e1b5b45efca96d2a847f45f327582
Paii <pyaepaikyaw@gmail.com>
2026-05-15 20:37:24 +0630

    feat: Add CategoryRepository to handle database tak only

---
+++ POS.Database/Repositories/CategoryRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POS.Database.Context;
+using POS.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Database.Repositories
+{
+    public class CategoryRepository
+    {
+        private readonly AppDbContext _dbContext;
+        public CategoryRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //GetAllCategories
+        public async Task<List<Category>> GetAllCategoriesAsync()
+        {
+            return await _dbContext.Categories.ToListAsync();
+        }
+
+        //GetById
+        public async Task<Category?> GetCategoryByIdAsync(int id)
+        {
+            return await _dbContext.Categories.FindAsync(id);
+        }
+
+        //Create
+        public async Task AddCategoryAsync(Category category)
+        {
+            _dbContext.Categories.Add(category);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        //Update
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            _dbContext.Categories.Update(category);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        //Delete
+        public async Task DeleteCategoryAsync(int id)
+        {
+            var category = await _dbContext.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _dbContext.Categories.Remove(category);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
