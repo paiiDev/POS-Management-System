fd77d74add2ee01da06ebba0634dd75cf8c16bf6
Paii <pyaepaikyaw@gmail.com>
2026-05-17 23:07:07 +0630

    feat: Add DTOs for mapping and implement CRUD features for category module.

--- POS.Database/Interfaces/ICategoryRepository.cs
+++ POS.Database/Interfaces/ICategoryRepository.cs
@@ -11,7 +11,7 @@ namespace POS.Database.Interfaces
     {
         Task<List<Category>> GetAllCategoriesAsync();
         Task<Category?> GetCategoryByIdAsync(int id);
-        Task<Category> AddCategoryAsync(Category category);
+        Task AddCategoryAsync(Category category);
         Task UpdateCategoryAsync(Category category);
         Task DeleteCategoryAsync(int id);
 
