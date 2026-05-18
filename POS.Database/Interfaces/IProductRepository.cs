8f00fb577e25c4342e954cff70f85b09e6ab163b
Paii <pyaepaikyaw@gmail.com>
2026-05-18 18:17:38 +0630

    Feat: Implementd Repository CRUD

---
+++ POS.Database/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using POS.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Database.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetAllProductsAsync();
+        Task<Entities.Product?> GetProductByIdAsync(int id);
+        Task CreateProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+    }
+}
---
+++ POS.Database/Repositories/ProductRepository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POS.Database.Context;
+using POS.Database.Entities;
+using POS.Database.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Database.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _dbContext;
+        public ProductRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public async Task<List<Product>> GetAllProductsAsync()
+        {
+            return await _dbContext.Products.AsNoTracking().Where(x => !x.IsDeleted).ToListAsync();
+        }
+
+
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+            return await _dbContext.Products.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id && !x.IsDeleted);
+        }
+
+
+        public async Task CreateProductAsync(Product product)
+        {
+             _dbContext.Products.Add(product);
+             await _dbContext.SaveChangesAsync();
+        }
+
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            var existingProduct = await _dbContext.Products.FirstOrDefaultAsync(x => x.Id == product.Id && !x.IsDeleted);
+            if (existingProduct != null)
+            {
+
+                existingProduct.Id = product.Id;
+                existingProduct.Name = product.Name;
+                existingProduct.Barcode = product.Barcode;
+                existingProduct.Price = product.Price;
+                existingProduct.StockQuantity = product.StockQuantity;
+                existingProduct.CategoryId = product.CategoryId;
+                await _dbContext.SaveChangesAsync();
+
+            }
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            var product = await _dbContext.Products.FirstOrDefaultAsync(x => x.Id == id && !x.IsDeleted);
+            if (product != null)
+            {
+                product.IsDeleted = true;
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
