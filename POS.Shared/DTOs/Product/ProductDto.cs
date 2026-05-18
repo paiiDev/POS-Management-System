b02e1202842ea0f4cac718dac822fcf421377ec7
Paii <pyaepaikyaw@gmail.com>
2026-05-18 18:17:07 +0630

    Feat: Add product DTOs

---
+++ POS.Shared/DTOs/Product/ProductDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Shared.DTOs.Product
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Barcode { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public int CategoryId { get; set; }
+
+
+    }
+}
