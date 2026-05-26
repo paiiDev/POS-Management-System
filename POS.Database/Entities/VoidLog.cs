78ff97acbc962915eb89e47d5053309fde8b4f79
Paii <pyaepaikyaw@gmail.com>
2026-05-26 21:03:21 +0630

    refactor: Added Status column to Sales table for better pos practice. Created new VoidLogs Table to handle Void sale.

---
+++ POS.Database/Entities/VoidLog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace POS.Database.Entities;
+
+public partial class VoidLog
+{
+    public int Id { get; set; }
+
+    public int SaleId { get; set; }
+
+    public int InvoiceNo { get; set; }
+
+    public decimal VoidedAmount { get; set; }
+
+    public string Reason { get; set; } = null!;
+
+    public DateTime VoidedAt { get; set; }
+
+    public string CashierName { get; set; } = null!;
+
+    public virtual Sale Sale { get; set; } = null!;
+}
